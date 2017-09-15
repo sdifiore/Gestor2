da91df8f1082b80bf3adb0e20144851bd0a7ca15
Sergio Di Fiore <difiores@outlook.com>
2017-09-15 15:53:59 -0300

    Migra;'ao inicial do sql server

--- Gestor/Models/IdentityModels.cs
+++ Gestor/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace Gestor.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("SqlServer", throwIfV1Schema: false)
         {
         }
 
